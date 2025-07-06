10344821e2673ef2817879b7ce65e777872e6b05
Rinolaringooto <portabilidad.zacatecas@gmail.com>
2025-07-06 01:23:18 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>¡Bienvenido a Next.js en Vercel!</h1>
+      <p>Esta es la página principal desplegada en Vercel.</p>
+      <a href="/about">Ir a Acerca de</a>
+    </div>
+  );
+}
