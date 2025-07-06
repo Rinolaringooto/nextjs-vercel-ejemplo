b1fc2e3ba1bce053268f906bef5e52e37581170d
Rinolaringooto <portabilidad.zacatecas@gmail.com>
2025-07-06 01:20:52 -0600

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Acerca de</h1>
+      <p>Esta es la página de acerca de. ¡Funciona en Vercel!</p>
+      <a href="/">Volver a la principal</a>
+    </div>
+  );
+}
